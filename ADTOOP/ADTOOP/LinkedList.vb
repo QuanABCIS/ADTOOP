7cbc4367a51f05b09a91f15b225e8a293038eed4
QuanABCIS <quanabcis2107@gmail.com>
2020-09-13 22:24:51 +0700

    Unordered linked list complete

--- ADTOOP/ADTOOP/LinkedList.vb
+++ ADTOOP/ADTOOP/LinkedList.vb
@@ -8,6 +8,58 @@
 
     Public Sub AddNode(data As Integer)
 
+        Dim nodeToAdd As New Node
+        nodeToAdd.SetData(data)
+
+        If IsNothing(Tail) Then
+            Tail = nodeToAdd
+        Else
+            TraverseAndAdd(nodeToAdd, Tail)
+        End If
+
+    End Sub
+
+    Private Sub TraverseAndAdd(nodeToAdd As Node, nodeToTraverse As Node)
+
+        If IsNothing(nodeToTraverse.GetNextNode) Then
+            nodeToTraverse.SetNextNode(nodeToAdd)
+        Else
+            TraverseAndAdd(nodeToAdd, nodeToTraverse.GetNextNode)
+        End If
+
+    End Sub
+
+    Public Sub FindNode(searchTerm As Integer)
+        TraverseAndFind(Tail, searchTerm)
+    End Sub
+
+    Private Function TraverseAndFind(nodeToTraverse As Node, searchTerm As Integer)
+        If nodeToTraverse.GetData = searchTerm Then
+            Console.WriteLine("Node Found")
+            Return True
+        ElseIf IsNothing(nodeToTraverse.GetNextNode) Then
+            Console.WriteLine("Node not Found")
+            Return True
+        Else
+            TraverseAndFind(nodeToTraverse.GetNextNode, searchTerm)
+        End If
+        Return True
+    End Function
+
+    Public Sub DeleteNode(deleteTerm As Integer)
+        TraverseAndDelete(Nothing, Tail, deleteTerm)
+    End Sub
+
+    Public Sub TraverseAndDelete(prevNode As Node, currentNode As Node, deleteTerm As Integer)
+
+        If currentNode.GetData = deleteTerm And IsNothing(prevNode) Then
+            Tail = currentNode.GetNextNode
+        ElseIf deleteTerm = currentNode.GetData Then
+            prevNode.SetNextNode(currentNode.GetNextNode)
+        Else
+            TraverseAndDelete(currentNode, currentNode.GetNextNode, deleteTerm)
+        End If
+
     End Sub
 
 End Class
--- ADTOOP/ADTOOP/Module1.vb
+++ ADTOOP/ADTOOP/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Dim ll As New LinkedList(10)
+        ll.AddNode(1)
+        ll.AddNode(2)
+        ll.AddNode(3)
+        ll.DeleteNode(1)
+        ll.FindNode(1)
+        Console.ReadLine()
     End Sub
 
 End Module
